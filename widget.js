e85b5134494630ae86af95401f3c17a004afcb5b
Anatolii Kozlov <toliyansky@gmail.com>
2025-01-10 02:42:43 +0200

    Old style

--- widget.js
+++ widget.js
@@ -1,12 +1,17 @@
 const TOKEN = "bf708a60d30c613b2bcef984dac36f0067222c0a";
 
-async function fetchAirQuality(stationId) {
+function fetchAirQuality(stationId, callback, errorCallback) {
     const url = "https://api.waqi.info/feed/" + stationId + "/?token=" + TOKEN;
-    const response = await new Request(url).loadJSON();
-    if (response.status !== "ok") {
-        throw new Error(`Failed to fetch air quality data for ${stationId}`);
-    }
-    return response.data;
+    const req = new Request(url);
+    req.loadJSON().then(function (response) {
+        if (response.status !== "ok") {
+            errorCallback(`Failed to fetch air quality data for ${stationId}`);
+        } else {
+            callback(response.data);
+        }
+    }).catch(function (error) {
+        errorCallback(error.message);
+    });
 }
 
 function getAQIColor(value) {
@@ -19,6 +24,7 @@ function getAQIColor(value) {
 }
 
 function formatUpdateTime(time) {
+    const date = new Date(time);
     const options = {
         timeZone: "Europe/Sofia",
         hour: "2-digit",
@@ -26,13 +32,13 @@ function formatUpdateTime(time) {
         second: "2-digit",
         hour12: false,
     };
-    return new Intl.DateTimeFormat("en-GB", options).format(new Date(time));
+    return new Intl.DateTimeFormat("en-GB", options).format(date);
 }
 
 function addAirQualitySection(widget, data, locationName) {
-    const pm25 = data.iaqi?.pm25?.v ?? "N/A";
-    const pm10 = data.iaqi?.pm10?.v ?? "N/A";
-    const lastUpdate = data.time?.iso ?? null;
+    const pm25 = data.iaqi && data.iaqi.pm25 ? data.iaqi.pm25.v : "N/A";
+    const pm10 = data.iaqi && data.iaqi.pm10 ? data.iaqi.pm10.v : "N/A";
+    const lastUpdate = data.time && data.time.iso ? data.time.iso : null;
 
     const locationTitle = widget.addText(locationName);
     locationTitle.font = Font.boldSystemFont(14);
@@ -40,11 +46,11 @@ function addAirQualitySection(widget, data, locationName) {
 
     widget.addSpacer(4);
 
-    const pm25Text = widget.addText(`PM2.5: ${pm25}`);
+    const pm25Text = widget.addText("PM2.5: " + pm25);
     pm25Text.font = Font.mediumSystemFont(14);
     pm25Text.textColor = typeof pm25 === "number" ? getAQIColor(pm25) : Color.gray();
 
-    const pm10Text = widget.addText(`PM10: ${pm10}`);
+    const pm10Text = widget.addText("PM10: " + pm10);
     pm10Text.font = Font.mediumSystemFont(14);
     pm10Text.textColor = typeof pm10 === "number" ? getAQIColor(pm10) : Color.gray();
 
@@ -52,7 +58,7 @@ function addAirQualitySection(widget, data, locationName) {
 
     if (lastUpdate) {
         const updateTime = formatUpdateTime(lastUpdate);
-        const updateText = widget.addText(`Updated: ${updateTime}`);
+        const updateText = widget.addText("Updated: " + updateTime);
         updateText.font = Font.mediumSystemFont(12);
         updateText.textColor = Color.gray();
     }
@@ -60,10 +66,8 @@ function addAirQualitySection(widget, data, locationName) {
     widget.addSpacer(8);
 }
 
-// Create widget
-async function createWidget() {
+function createWidget() {
     const homeStationId = "A232735"; // Example station ID
-
     const widget = new ListWidget();
     widget.backgroundColor = new Color("#1c1c1e");
 
@@ -73,25 +77,32 @@ async function createWidget() {
 
     widget.addSpacer(8);
 
-    try {
-        const stationData = await fetchAirQuality(homeStationId);
-        addAirQualitySection(widget, stationData, `Station: ${stationData.city.name}`);
-    } catch (error) {
+    fetchAirQuality(homeStationId, function (stationData) {
+        addAirQualitySection(widget, stationData, "Station: " + stationData.city.name);
+        if (config.runsInWidget) {
+            Script.setWidget(widget);
+            Script.complete();
+        } else {
+            widget.presentMedium();
+        }
+    }, function (error) {
         const errorText = widget.addText("Failed to fetch station data");
         errorText.font = Font.mediumSystemFont(12);
         errorText.textColor = Color.red();
         widget.addSpacer(8);
-    }
+
+        if (config.runsInWidget) {
+            Script.setWidget(widget);
+            Script.complete();
+        } else {
+            widget.presentMedium();
+        }
+    });
 
     return widget;
 }
 
 // Main execution
-if (config.runsInWidget) {
-    const widget = await createWidget();
-    Script.setWidget(widget);
-    Script.complete();
-} else {
-    const widget = await createWidget();
-    widget.presentMedium();
+if (!config.runsInWidget) {
+    createWidget();
 }
