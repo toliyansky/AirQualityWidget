2dd23e9e182879690fb128f9c140919fcd60c1a0
Anatolii Kozlov <anatolii.kozlov@ringcentral.com>
2025-01-11 14:45:33 +0200

    Revert "Average"
    This reverts commit 1adf122c073ed3870ada7b0e886195e370a52623.

--- widget.js
+++ widget.js
@@ -14,36 +14,25 @@ function fetchAirQuality(stationId, callback, errorCallback) {
     });
 }
 
-function fetchNearbyStations(lat, lon, callback, errorCallback) {
-    const url = `https://api.waqi.info/map/bounds/?latlng=${lat - 0.01},${lon - 0.01},${lat + 0.01},${lon + 0.01}&token=${TOKEN}`;
-    const req = new Request(url);
-    req.loadJSON().then(function (response) {
-        if (response.status !== "ok") {
-            errorCallback("Failed to fetch nearby stations.");
-        } else {
-            callback(response.data);
-        }
-    }).catch(function (error) {
-        errorCallback(error.message);
-    });
+function getAQIColor(value) {
+    if (value <= 50) return new Color("#00e400"); // Good
+    if (value <= 100) return new Color("#ffff00"); // Moderate
+    if (value <= 150) return new Color("#ff7e00"); // Unhealthy for Sensitive Groups
+    if (value <= 200) return new Color("#ff0000"); // Unhealthy
+    if (value <= 300) return new Color("#99004c"); // Very Unhealthy
+    return new Color("#7e0023"); // Hazardous
 }
 
-function calculateAveragePM(stations) {
-    let pm25Sum = 0, pm10Sum = 0, pm25Count = 0, pm10Count = 0;
-    stations.forEach(station => {
-        if (station.iaqi?.pm25?.v) {
-            pm25Sum += station.iaqi.pm25.v;
-            pm25Count++;
-        }
-        if (station.iaqi?.pm10?.v) {
-            pm10Sum += station.iaqi.pm10.v;
-            pm10Count++;
-        }
-    });
-    return {
-        avgPM25: pm25Count > 0 ? (pm25Sum / pm25Count).toFixed(1) : "N/A",
-        avgPM10: pm10Count > 0 ? (pm10Sum / pm10Count).toFixed(1) : "N/A"
+function formatUpdateTime(time) {
+    const date = new Date(time);
+    const options = {
+        timeZone: "Europe/Sofia",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
     };
+    return new Intl.DateTimeFormat("en-GB", options).format(date);
 }
 
 function addAirQualitySection(widget, data, locationName) {
@@ -77,24 +66,6 @@ function addAirQualitySection(widget, data, locationName) {
     widget.addSpacer(8);
 }
 
-function addAverageAirQualitySection(widget, avgData) {
-    const avgTitle = widget.addText("Zone Average:");
-    avgTitle.font = Font.boldSystemFont(14);
-    avgTitle.textColor = Color.white();
-
-    widget.addSpacer(4);
-
-    const avgPM25Text = widget.addText("PM2.5: " + avgData.avgPM25);
-    avgPM25Text.font = Font.mediumSystemFont(14);
-    avgPM25Text.textColor = typeof avgData.avgPM25 === "number" ? getAQIColor(avgData.avgPM25) : Color.gray();
-
-    const avgPM10Text = widget.addText("PM10: " + avgData.avgPM10);
-    avgPM10Text.font = Font.mediumSystemFont(14);
-    avgPM10Text.textColor = typeof avgData.avgPM10 === "number" ? getAQIColor(avgData.avgPM10) : Color.gray();
-
-    widget.addSpacer(8);
-}
-
 function createWidget() {
     const homeStationId = "A232735"; // Example station ID
     const widget = new ListWidget();
@@ -107,37 +78,25 @@ function createWidget() {
     widget.addSpacer(8);
 
     fetchAirQuality(homeStationId, function (stationData) {
-        const leftStack = widget.addStack();
-        const rightStack = widget.addStack();
-
-        leftStack.layoutVertically();
-        rightStack.layoutVertically();
-
-        addAirQualitySection(leftStack, stationData, "Station: " + stationData.city.name);
-
-        const lat = stationData.city.geo[0];
-        const lon = stationData.city.geo[1];
-
-        fetchNearbyStations(lat, lon, function (stations) {
-            const avgData = calculateAveragePM(stations);
-            addAverageAirQualitySection(rightStack, avgData);
-
-            if (config.runsInWidget) {
-                Script.setWidget(widget);
-                Script.complete();
-            } else {
-                widget.presentMedium();
-            }
-        }, function (error) {
-            const errorText = widget.addText("Failed to fetch nearby stations");
-            errorText.font = Font.mediumSystemFont(12);
-            errorText.textColor = Color.red();
-        });
-
+        addAirQualitySection(widget, stationData, "Station: " + stationData.city.name);
+        if (config.runsInWidget) {
+            Script.setWidget(widget);
+            Script.complete();
+        } else {
+            widget.presentMedium();
+        }
     }, function (error) {
         const errorText = widget.addText("Failed to fetch station data");
         errorText.font = Font.mediumSystemFont(12);
         errorText.textColor = Color.red();
+        widget.addSpacer(8);
+
+        if (config.runsInWidget) {
+            Script.setWidget(widget);
+            Script.complete();
+        } else {
+            widget.presentMedium();
+        }
     });
 
     return widget;
